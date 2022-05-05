34cf0d9ad95c80afa937b35078e8e4243e113516
Venkatesh2304 <96430022+Venkatesh2304@users.noreply.github.com>
2022-05-05 16:12:15 +0530

    outtsanding breakup

--- creditlock.py
+++ creditlock.py
@@ -38,7 +38,11 @@ def getlockdetails(driver,party_data) :
   for key,value in party_data.items() : 
       url_rec = url_rec.replace('_'+key+'_',str(value))
   req = ajax_plain(driver,url_rec)  
-  return { "billsutilised" : req["creditBillsUtilised"] , "creditlimit": req["creditLimitUtilised"] } 
+  outstanding = req["collectionPendingBillVOList"] 
+  breakup = [ [bill["pendingDays"],bill["outstanding"]]  for bill in outstanding ]
+  breakup.sort(key=lambda x: x[0],reverse=True)
+  breakup = "/".join( [ str(bill[0])+"*"+str(bill[1]) for bill in breakup ] )
+  return { "billsutilised" : req["creditBillsUtilised"] , "creditlimit": breakup } 
 
 def releaselock(driver,party_data) :  
   # party_data = { "parCode": "P-P18078" , "parCodeHll": "HUL-413724D-P3364","parCodeRef": "D-P18078" , "parId": 5582 }
